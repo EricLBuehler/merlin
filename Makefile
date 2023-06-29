b055f6ccb1f9b3f18772303e24c4a8537463d7eb
EricLBuehler <ericlbuehler@gmail.com>
2023-06-28 22:04:16 -0400

    Add Trc initial

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 default:
-	g++ main.cpp -o merlin -Wall -Wpedantic -std=c++23
+	g++ src/main.cpp -o merlin -Wall -Wpedantic -std=c++23
 clean:
 	rm merlin
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "termcolor/termcolor.hpp"
-
-int main(int argc, char** argv) {
-    std::u8string str = u8"This is a u8 string\n";
-    std::cout << termcolor::green << "Welcome to Merlin!\n" << termcolor::reset;
-    std::cout << reinterpret_cast<const char*>(str.c_str());
-}
